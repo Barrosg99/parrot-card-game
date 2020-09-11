fd7c9ea5fb7a2eaa63a9550eaaba81a1d5c44df9
Gabriel Barros <barros.fontes@unesp.br>
2020-09-11 04:41:08 -0300

    clique na carta inteiramente aplicado

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var n=0;
 var ale = [];
+var anterior = document.querySelector("main");
+var main = document.querySelector("main");
+var cartaTras;
+var cartaFrente;
+var trancaMesa = true;
 function nDeCartas()
 {
     while(n%2!=0 || n<4 || n>14)
@@ -59,10 +64,17 @@ function DistribCartasFrente()
 }
 function viraCarta(elemento)
 {
-    var cartaTras = elemento.children[0];
-    var cartaFrente = elemento.children[1];
-    cartaTras.classList.toggle("carta-tras-virada");
-    cartaFrente.classList.toggle("carta-frente-virada");
+    if(trancaMesa)
+    {
+        cartaTras = elemento.children[0];
+        cartaFrente = elemento.children[1];
+        if(verificaCarta(elemento))
+        {
+            cartaTras.classList.toggle("carta-tras-virada");
+            cartaFrente.classList.toggle("carta-frente-virada");
+        }
+    }
+        
 }
 function embaralha()
 {
@@ -82,12 +94,9 @@ function papagaioAleatorio()
     {
         tamanho[i] = embaralhado[i];
         i++;
-    }
-    console.log(tamanho)        
+    }  
     ale = concatenarArrays(tamanho,tamanho);
-    console.log(ale);
     ale = ale.sort(embaralha);
-    console.log(ale);
 }
 function concatenarArrays(lista1, lista2) {
     var i = 0;
@@ -98,6 +107,55 @@ function concatenarArrays(lista1, lista2) {
     }
     return lista1;
   }
+function verificaCarta(elemento)
+{
+    cartaTras = elemento.children[0];
+    cartaFrente = elemento.children[1];
+    if(cartaTras.classList.contains("carta-tras-virada"))
+    {
+        console.log("nao clica na msm carta animal")
+        anterior = cartaFrente;
+        return false;
+    }
+    else if (anterior.innerHTML!==main.innerHTML)
+    {
+        if(anterior.innerHTML===cartaFrente.innerHTML)
+        {
+            console.log("entao vc achou o par? brabo")
+            anterior.parentElement.removeAttribute("onclick");
+            elemento.removeAttribute("onclick");
+            anterior = document.querySelector("main");
+            return true;
+        }
+        else
+        {
+            cartaTras.classList.toggle("carta-tras-virada");
+            cartaFrente.classList.toggle("carta-frente-virada");
+            setTimeout(esperar1seg, 1000);
+            trancaMesa = false;
+            console.log("nao achou, lixo")
+            return false;
+        }
+    }
+    else
+    {
+        console.log("outros casos")
+        anterior = cartaFrente;
+        return true;
+    } 
+        
+}
+function esperar1seg()
+{
+    cartaTras.classList.toggle("carta-tras-virada");
+    cartaFrente.classList.toggle("carta-frente-virada");
+    var anteriorAtras = anterior.parentElement.children[0];
+    anteriorAtras.classList.toggle("carta-tras-virada")
+    anterior.classList.toggle("carta-frente-virada")
+    anterior = document.querySelector("main");
+    console.log("pera ae corno");
+    trancaMesa = true;
+}
 nDeCartas();
 montarJogo();
 DistribCartasTras();
